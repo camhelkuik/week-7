b09fe17f5d0852c91eab77682debfd832164c8da
Cameron Helkuik <cameron@travefy.com>
2019-04-23 10:42:21 -0500

    Initial commit

---
+++ Assignment7/Assignment7/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace Assignment7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNumber = new System.Windows.Forms.TextBox();
+            this.txtList = new System.Windows.Forms.TextBox();
+            this.btnAddNumber = new System.Windows.Forms.Button();
+            this.btnView = new System.Windows.Forms.Button();
+            this.btnSort = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(125, 102);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(144, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter Number";
+            // 
+            // txtNumber
+            // 
+            this.txtNumber.Location = new System.Drawing.Point(329, 96);
+            this.txtNumber.Name = "txtNumber";
+            this.txtNumber.Size = new System.Drawing.Size(210, 31);
+            this.txtNumber.TabIndex = 1;
+            // 
+            // txtList
+            // 
+            this.txtList.Location = new System.Drawing.Point(130, 244);
+            this.txtList.Multiline = true;
+            this.txtList.Name = "txtList";
+            this.txtList.Size = new System.Drawing.Size(567, 488);
+            this.txtList.TabIndex = 2;
+            // 
+            // btnAddNumber
+            // 
+            this.btnAddNumber.Location = new System.Drawing.Point(130, 176);
+            this.btnAddNumber.Name = "btnAddNumber";
+            this.btnAddNumber.Size = new System.Drawing.Size(170, 42);
+            this.btnAddNumber.TabIndex = 3;
+            this.btnAddNumber.Text = "&Add Number";
+            this.btnAddNumber.UseVisualStyleBackColor = true;
+            this.btnAddNumber.Click += new System.EventHandler(this.btnAddNumber_Click);
+            // 
+            // btnView
+            // 
+            this.btnView.Location = new System.Drawing.Point(329, 176);
+            this.btnView.Name = "btnView";
+            this.btnView.Size = new System.Drawing.Size(170, 42);
+            this.btnView.TabIndex = 4;
+            this.btnView.Text = "&View";
+            this.btnView.UseVisualStyleBackColor = true;
+            this.btnView.Click += new System.EventHandler(this.btnView_Click);
+            // 
+            // btnSort
+            // 
+            this.btnSort.Location = new System.Drawing.Point(527, 176);
+            this.btnSort.Name = "btnSort";
+            this.btnSort.Size = new System.Drawing.Size(170, 42);
+            this.btnSort.TabIndex = 5;
+            this.btnSort.Text = "&Sort";
+            this.btnSort.UseVisualStyleBackColor = true;
+            this.btnSort.Click += new System.EventHandler(this.btnSort_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(221, 801);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(170, 42);
+            this.btnClear.TabIndex = 6;
+            this.btnClear.Text = "&Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(443, 801);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(170, 42);
+            this.btnExit.TabIndex = 7;
+            this.btnExit.Text = "E&xit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(825, 950);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnSort);
+            this.Controls.Add(this.btnView);
+            this.Controls.Add(this.btnAddNumber);
+            this.Controls.Add(this.txtList);
+            this.Controls.Add(this.txtNumber);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNumber;
+        private System.Windows.Forms.TextBox txtList;
+        private System.Windows.Forms.Button btnAddNumber;
+        private System.Windows.Forms.Button btnView;
+        private System.Windows.Forms.Button btnSort;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ Assignment7/Assignment7/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace Assignment7
+{
+    public partial class Form1 : Form
+    {
+        List<int> numbers = new List<int>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Text = "Bubble Sort - Cameron Helkuik";
+        }
+
+        private void btnAddNumber_Click(object sender, EventArgs e)
+        {
+            var newNumber = int.Parse(txtNumber.Text.ToString());
+            numbers.Add(newNumber);
+
+            txtNumber.Text = "";
+        }
+
+        private void btnView_Click(object sender, EventArgs e)
+        {
+            txtList.Text = string.Join(", ", numbers);
+        }
+
+        private void btnSort_Click(object sender, EventArgs e)
+        {
+            int temp;
+            int numberOfSwaps = 0;
+            var count = numbers.ToArray();
+
+            for ( int x = 0; x < count.Length - 1; x++)
+            {
+                for ( int i = 0; i < count.Length - x - 1; i++)
+                {
+                    if (count[i] > count[i + 1])
+                    {
+                        temp = count[i + 1];
+                        count[i + 1] = count[i];
+                        count[i] = temp;
+
+                        numberOfSwaps++;
+                    }
+                }
+
+                //check number of swaps, if 0 no need to keep going, else reset the number of swaps
+                if (numberOfSwaps == 0)
+                {
+                    break;
+                }
+                else
+                {
+                    numberOfSwaps = 0;
+                }
+            }
+
+            txtList.Text = string.Join(", ", count);
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtList.Text = "";
+            numbers.Clear();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+    }
+}
